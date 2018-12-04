3aa817f422400d3f3392cadade6abe4387cf73d2
fractalwrench <fractalwrench@gmail.com>
2018-12-04 21:01:36 +0000

    pass day 3a

--- src/main/java/com/fractalwrench/aoc/Day3A.kt
+++ src/main/java/com/fractalwrench/aoc/Day3A.kt
@@ -5,14 +5,33 @@ fun main(args: Array<String>) {
     println(solveDay3A(input))
 }
 
-fun solveDay3A(input: String): String {
+fun solveDay3A(input: String): Int {
     val claims = input.lines().map(::parseClaimLine)
+    val matrix = generateEmptyMatrix(claims)
 
-    return ""
+    claims.forEach {
+        mutateMatrix(matrix, it.coords)
+    }
+
+    return matrix.map { row -> row.count { it > 1 } }.sum()
+}
+
+private fun mutateMatrix(matrix: Array<Array<Int>>, coords: Claim.Coords) {
+    for (k in coords.top until coords.bottom) {
+        for (n in coords.left until coords.right) {
+            matrix[k][n]++
+        }
+    }
+}
+
+private fun generateEmptyMatrix(claims: List<Claim>): Array<Array<Int>> {
+    val maxRight = claims.map { it.coords.right }.max()!!
+    val maxBottom = claims.map { it.coords.bottom }.max()!!
+    return Array(maxBottom) { Array(maxRight) { 0 } }
 }
 
 internal fun parseClaimLine(input: String): Claim {
-    val split = "#123 @ 3,2: 5x4".split(" ")
+    val split = input.split(" ")
     val id = split[0].substring(1).toInt()
     val startPos = split[2].removeSuffix(":").split(",").map(String::toInt)
     val size = split[3].split("x").map(String::toInt)
